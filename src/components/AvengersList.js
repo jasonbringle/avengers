88f2dc4d184be9401d691c95d65354fd4d964855
Jason <Jason@MacBook-Pro-3.local>
2020-02-12 23:18:27 -0800

    Added styling

--- src/components/AvengersList.js
+++ src/components/AvengersList.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import avengers from './avengersData'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components'
 
+const Linky = styled(Link)`
+text-decoration:none;
+`
 
 const AvengersList = (props) => {
     return (
       
       <div className="posts-container-wrapper">
         {avengers.map((character,index)=>{
-            return <Link key={index} to={`/avengers/${character.id}`}>
+            return <Linky key={index} to={`/avengers/${character.id}`}>
             <h3>{character.name}</h3>
-          </Link>
+          </Linky>
             })}
             
       </div>
